6793d540a060c67d1569321190c4cf250e852cd3
kasumi sogabe <ksm17sgb@gmail.com>
2023-02-14 18:14:17 +0900

    pictureのCRUD機能実装

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,59 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+    return render :new if params[:back]
+    if @picture.save
+      redirect_to pictures_path, notice: '画像を投稿しました'
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: '投稿を編集しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to picturess_path, notice:"ブログを削除しました！"
+  end
+
+  def confirm
+    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :content, :image, :image_cache)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
   resources :sessions
   resources :users
 end
