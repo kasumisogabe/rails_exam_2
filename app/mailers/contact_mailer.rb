b9a9aa590444cf6f9e4b78c524731b49405d8d96
kasumi sogabe <ksm17sgb@gmail.com>
2023-02-15 20:59:15 +0900

    メールアドレス再設定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(picture)
     @picture = picture
 
-    mail to: ksm17sgb@gmail.com, subject: "画像投稿完了メール"
+    mail to: @picture.user.email, subject: "画像投稿完了メール"
   end
 end
