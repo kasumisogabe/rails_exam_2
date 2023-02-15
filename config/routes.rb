cbec1a09419916076e0430823f6c9e76b46655f2
kasumi sogabe <ksm17sgb@gmail.com>
2023-02-16 01:31:40 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites
   resources :sessions
   resources :users
 end
