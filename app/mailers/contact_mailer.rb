518ea37dd4e4addd2c84c135c6fc45c20bcc874a
kasumi sogabe <ksm17sgb@gmail.com>
2023-02-15 20:31:31 +0900

    メールアドレスの記載変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(picture)
     @picture = picture
 
-    mail to: @picture.email, subject: "画像投稿完了メール"
+    mail to: ksm17sgb@gmail.com, subject: "画像投稿完了メール"
   end
 end
